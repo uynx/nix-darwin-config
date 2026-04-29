95723da13f449d0e5dfe116c715d63a6c56bb176
Brandon Alexander <brandonwalex@pm.me>
2026-04-29 14:47:27 -0400

    Package Updates.

--- dotfiles/brave-activation.sh
+++ dotfiles/brave-activation.sh
@@ -9,14 +9,12 @@ export PATH="/Users/uynx/.nix-profile/bin:/opt/homebrew/bin:/usr/local/bin:$PATH
 TARGET_PROFILE_DIR=$1
 PROFILE_NAME=$2
 
-# Focus existing window if available (via aerospace)
 WINDOW_ID=$(aerospace list-windows --all | tac | awk -v suffix="Brave - $PROFILE_NAME" '$0 ~ suffix"$" {print $1; exit}')
 
 if [ -n "$WINDOW_ID" ]; then
   aerospace focus --window-id "$WINDOW_ID"
   brave --new-window --profile-directory="$TARGET_PROFILE_DIR"
 else
-  # Launch with lean, anonymity-focused flags (Persistence enabled: no --incognito)
   brave --profile-directory="$TARGET_PROFILE_DIR" \
     --disable-breakpad \
     --no-pings \
